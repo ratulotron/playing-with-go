cc6d02d00392b01c5af95a4bec055abf5e399602
ratulotron <m@ratul.xyz>
2017-12-21 18:16:24 +0600

    ki jani korsi

---
+++ toptal-step-by-step/funding/fund_test.go
@@ -0,0 +1,56 @@
+package funding
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+const WORKERS = 10
+
+func BenchmarkWithdrawals(b *testing.B) {
+	// Skip N = 1
+	if b.N < WORKERS {
+		return
+	}
+
+	// Add as many dollars as we have iterations this run
+	fund := NewFund(b.N)
+
+	// Casually assume b.N divides cleanly
+	dollarsPerFounder := b.N / WORKERS
+
+	// WaitGroup structs don't need to be initialized
+	// (their "zero value" is ready to use).
+	// So, we just declare one and then use it.
+	var wg sync.WaitGroup
+
+	for i := 0; i < WORKERS; i++ {
+		// Let the waitgroup know we're adding a goroutine
+		wg.Add(1)
+		fmt.Println("Added worker no.", i+1)
+
+		// Spawn off a founder worker, as a closure
+		go func() {
+			// Mark this worker done when the function finishes
+			defer wg.Done()
+
+			for i := 0; i < dollarsPerFounder; i++ {
+				fund.Withdraw(1)
+			}
+
+			fmt.Println("Withdrawal done")
+		}() // Remember to call the closure!
+	}
+
+	// Wait for all the workers to finish
+	wg.Wait()
+	fmt.Println("Waiting for all workers to finish")
+
+	if fund.Balance() != 0 {
+		b.Error("Balance wasn't zero:", fund.Balance())
+	}
+
+	fmt.Println("Finished")
+	fmt.Println("Final balance:", fund.balance)
+}
