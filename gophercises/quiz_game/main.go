ff9aecc52fda9efa80ab569bed95f96447cc372b
ratulotron <m@ratul.xyz>
2018-02-04 01:59:40 +0600

    https://gophercises.com/exercises/quiz

---
+++ gophercises/quiz_game/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
+
+type Question struct {
+	Question  string
+	Answer    string
+	IsCorrect bool
+}
+
+// check is an error handler
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func loadQuestions(filename string) (problems []Question) {
+	file, err := os.Open(filename)
+	check(err)
+	// Close the file at the end of the program
+	defer file.Close()
+
+	// Read File into a variable
+	lines, err := csv.NewReader(file).ReadAll()
+	check(err)
+
+	// Loop through lines & turn into object
+	for _, line := range lines {
+		question := Question{
+			Question: line[0],
+			Answer:   line[1],
+		}
+		problems = append(problems, question)
+	}
+	return problems
+}
+
+func askQuestion(question *Question) (isCorrect bool) {
+	fmt.Print(question.Question, " = ")
+	var answer string
+	_, err := fmt.Scan(&answer)
+	check(err)
+
+	if question.Answer == answer {
+		//fmt.Println("Oh yes")
+		question.IsCorrect = true
+		// } else {
+		// 	fmt.Println("WTF man")
+	}
+
+	return isCorrect
+}
+
+func main() {
+	// flag, default, description
+	filename := flag.String("word", "test.csv", "a string")
+	// Unless this is called, command-line flags aren't really parsed
+	flag.Parse()
+
+	// Loads all the problems in the program
+	questions := loadQuestions(*filename)
+	// fmt.Println(questions[0])
+	// askQuestion(&questions[0])
+	// fmt.Println(questions[0])
+
+	for key, question := range questions {
+		fmt.Printf("Problem #%d: ", key+1)
+		askQuestion(&question)
+	}
+
+	fmt.Printf(questions)
+}
